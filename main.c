b3c86027d0e662082d994ab41c76aa3eef0c4f66
zuoyinuo22 <2809810038@qq.com>
2024-11-16 23:00:14 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
 //请在下方输入你的代码：
+#include "stdio.h"
+int main() {
+    for (int i = 1; i <= 9; i++) {
+        for (int p = 1; p <= i; p++) {
+            printf("%d*%d=%d ", p, i, p * i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
