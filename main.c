bbf7714f72e9679bac80f5006b5ad96c224c798c
zuoyinuo22 <2809810038@qq.com>
2024-11-17 21:47:34 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 //请在下方输入你的代码：
 #include "stdio.h"
 int main() {
+    //正三角
     for (int i = 1; i <= 9; i++) {
         for (int p = 1; p <= i; p++) {
+            printf("%d*%d=%d ", p, i, p * i);
+        }
+        printf("\n");
+    }
+    //倒三角
+    for (int i = 1; i <= 9; i++) {
+        for (int p = 9; p >= i; p--) {
             printf("%d*%d=%d ", p, i, p * i);
         }
         printf("\n");
